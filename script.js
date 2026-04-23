b24dac4d85d7e7666fd4560c8d31181737560b3a
Gaurav Vitthal Ingawale <ingwalegaurav7@gmail.com>
2026-04-23 14:51:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Smooth scroll
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+// Scroll animation
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      sec.classList.add("show");
+    }
+  });
+});
